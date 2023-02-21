145f7d427594b7f7e8ddd7ecd3e2ded8b8a9b0b3
Dustin Gilmore <21026373+gilmored@users.noreply.github.com>
2023-02-21 16:41:32 -0600

    prompts user for info to create username and password. Functional but currently stores the password as plain text

--- capstone.py
+++ capstone.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import DeclarativeBase
-# from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-# Base = declarative_base()
 class Base(DeclarativeBase):
     pass
 
@@ -14,21 +12,24 @@ class User(Base):
     username = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
 
-def __init__(self, username, password):
-    self.username = username
-    self.password = password
+# Creates user   // currently stores password in plain text
+def create_user():
+    engine = create_engine('sqlite:///users.db', echo=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    user = User(username=username, password=password)
+    session.add(user)
+    session.commit()
+    print("User created successfully.")
 
-engine = create_engine('sqlite:///users.db', echo=True)
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
-
-# user1 = User(username='mike', password='pass123')
-# session.add(user1)
-# session.commit()
+#Test to query the database for all users
+    # results = session.query(User).all()
+    # for result in results:
+    #     print(result.username, result.password)
+create_user()
 
 
-######################################
 
-results = session.query(User).all()
-print(results)
